c765d5f534db1e02a04622f6094ce2e7aff74b9b
eswaribala <parameswaribala@gmail.com>
2022-03-09 11:54:31 +0530

    updated

---
+++ globalinsurance/src/main/java/com/vusbc/globalinsurance/LoggerDelegate.java
@@ -0,0 +1,42 @@
+package com.vusbc.globalinsurance;
+
+import java.util.logging.Logger;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.vusbc.globalinsurance.services.PolicySearchService;
+
+/**
+ * This is an easy adapter implementation
+ * illustrating how a Java Delegate can be used
+ * from within a BPMN 2.0 Service Task.
+ */
+@Component("logger")
+public class LoggerDelegate implements JavaDelegate {
+ 
+  @Autowired	
+  private PolicySearchService policySearchService;	
+  private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
+  
+  public void execute(DelegateExecution execution) throws Exception {
+    
+    LOGGER.info("\n\n  ... LoggerDelegate invoked by "
+            + "activityName='" + execution.getCurrentActivityName() + "'"
+            + ", activityId=" + execution.getCurrentActivityId()
+            + ", processDefinitionId=" + execution.getProcessDefinitionId()
+            + ", processInstanceId=" + execution.getProcessInstanceId()
+            + ", businessKey=" + execution.getProcessBusinessKey()
+            + ", executionId=" + execution.getId()
+            + ", variables=" + execution.getVariables()
+            + " \n\n");
+    long policyNo=Long.parseLong(execution.getVariable("policyNo").toString());
+    policySearchService.publishPolicyNo(policyNo);
+    		
+    
+    execution.setVariable("status", true);
+  }
+
+}
